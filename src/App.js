4214e1409e89a74a3a7fa5fb27803c71542ed944
ccpretorius <84196590+ccpretorius@users.noreply.github.com>
2024-03-07 22:29:37 +0100

    Passing default props when not passing props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  const comments = [
-    { id: 1, text: "Comment 1" },
-    { id: 2, text: "Comment 2" },
-    { id: 3, text: "Comment 3" },
-  ];
   return (
     <>
-      <h1>Hello From the app component</h1>
-      <h2>Hello again</h2>
-      <ul>
-        <div className="comments">
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </div>
-      </ul>
+      <Header />
+      <div className="container">
+        <h1>My APP</h1>;
+      </div>
     </>
   );
 }
