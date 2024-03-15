ca928dceb3759b04d9a154ce460bb21a280bcab1
ccpretorius <84196590+ccpretorius@users.noreply.github.com>
2024-03-15 22:47:56 +0100

    Fetch data from json-server backend and setting - async await not working - replaced with .then.catch blocks

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,31 +1,43 @@
 import { v4 as uuidv4 } from "uuid";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This item 1 is from context",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This item 2 is from context",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This item 3 is from context",
-      rating: 7,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  // SOMEHOW acync await is not working here, but .then.catch is
+  // const fetchFeedback = async () => {
+  //   const response = await fetch("http://localhost:5001/feedback?_sort=id&_order=desc");
+  //   console.log(response); // Log the raw response
+  //   const data = await response.json();
+  //   console.log(data); // Log the fetched data
+
+  //   setFeedback(data);
+  //   console.log(data); // Log the fetched data
+  // };
+  // Simplified fetch feedback without async
+  const fetchFeedback = () => {
+    fetch("http://localhost:5001/feedback")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Fetched data:", data);
+        setFeedback(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  };
 
   // Add feedback
   const addFeedback = (newFeedback) => {
@@ -59,6 +71,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback: feedback, //could be just feedback
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
